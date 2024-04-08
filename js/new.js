9d95e6e3f36eff8cbf3fe0c1928a4d525988d9fa
DinaSharipova <sharipova0308@gmail.com>
2024-04-08 15:43:46 +0500

    add filter mob

--- js/new.js
+++ js/new.js
@@ -1,19 +1,21 @@
 //меняются стили фильтра при прокрутке
-$(window).scroll(function() {
+if ($(window).width() > 747) {
+    $(window).scroll(function() {
     
-    if ($(this).scrollTop() > 300) { 
-        $('#filters').addClass('filter-top'); 
-        $('.table').css('padding-top', '150px');
+        if ($(this).scrollTop() > 300) { 
+            $('#filters').addClass('filter-top'); 
+            $('.table').css('padding-top', '150px');
+            
+            
+        } else {
+            $('#filters').removeClass('filter-top'); 
+            $('.table').css('padding-top', '0');
+            
+        }
+    });
+    $('.change-filters').on('click', function() {
         
+        $('.filter-top').toggleClass('filter-top-show');
         
-    } else {
-        $('#filters').removeClass('filter-top'); 
-        $('.table').css('padding-top', '0');
-        
-    }
-});
-$('.change-filters').on('click', function() {
-    
-    $('.filter-top').toggleClass('filter-top-show');
-    
-});
+    });
+}

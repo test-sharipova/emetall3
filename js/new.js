c0fbb896168eb4f6f85c9c23b41fbb4a595492e6
DinaSharipova <sharipova0308@gmail.com>
2024-04-10 15:44:28 +0500

    add profile

--- js/new.js
+++ js/new.js
@@ -30,6 +30,19 @@ $('.catalogFilters__close').on('click', function(){
     document.body.style.position = '';
     document.body.style.width = '';
 });
+//главная меню личного кабинета
+$('.header__item__profile').on('click', function(){
+    $('.header__item__profile__menu').toggleClass('header__item__profile__menu-active');
+
+});
+$(document).mouseup( function(e){ 
+		var div = $( ".header__item__profile" ); 
+		if ( !div.is(e.target) // если клик был не по нашему блоку
+		    && div.has(e.target).length === 0 ) { // и не по его дочерним элементам
+                $('.header__item__profile__menu').removeClass('header__item__profile__menu-active'); // скрываем его
+		}
+	});
+    
 //закрыть окно выбора диаметра
 $('.close-diametr').on('click', function(){
     $('.select__wrap_diametr').removeClass('opened');
